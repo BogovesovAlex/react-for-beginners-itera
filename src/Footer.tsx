62bdfedd610fd42660608f0162709d3b0739e2ff
BogovesovAlex <bogovesov.alex@gmail.com>
2022-05-13 14:32:50 +0300

    add project

---
+++ src/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export type FooterProps = {
+  copyright: string;
+}
+
+export const Footer: React.FC<FooterProps> = (props) => (
+  <footer>Footer {props.copyright}</footer>
+);
